125fe81464096a57c69f0bad507f14c867fd656c
maxiangsai <maxiangsai@163.com>
2021-06-18 00:57:42 +0800

    docs: 地址

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # realworld-nuxtjs
 
+[线上地址](http://47.113.89.113:3000)
 ## Build Setup
 
 ```bash
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+  server: {
+    host: '0.0.0.0',
+  },
   head: {
     title: 'realworld-nuxtjs',
     htmlAttrs: {
